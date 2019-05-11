01d59e776f1deebd336d0f03bd1e1589d042ce2e
JaysusM <jmartinruiz@uma.es>
2019-05-11 17:40:04 +0200

    Add forgot password > sent email

---
+++ Socia-Lite/Socia-Lite-war/src/java/socialite/servlet/ForgetPasswordServlet.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socialite.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import socialite.dao.PasswordResetFacade;
+import socialite.dao.UserFacade;
+import socialite.entity.PasswordReset;
+import socialite.entity.User;
+
+/**
+ *
+ * @author jaysus
+ */
+@WebServlet(name = "ForgetPasswordServlet", urlPatterns = {"/ForgetPasswordServlet"})
+public class ForgetPasswordServlet extends HttpServlet {
+
+    @EJB
+    private UserFacade userFacade;
+    
+    @EJB
+    private PasswordResetFacade passwordResetFacade;
+    
+    private final String RESET_BASE_URL = "http://localhost:8080/Socia-Lite-war/ResetPasswordServlet?resetId=";
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        User user = userFacade.findByEmail(email);
+        if(user == null) {
+            request.setAttribute("error", true);
+        } else {
+            PasswordReset psswdReset = new PasswordReset();
+            psswdReset.setUsuario(user);
+            
+            Calendar c = Calendar.getInstance();
+            c.setTime(new Date());
+            c.add(Calendar.HOUR, 1);
+            psswdReset.setExpiritionDate(c.getTime());
+            
+            String resetUrlId = UUID.randomUUID().toString();
+            
+            String resetUrl = RESET_BASE_URL + resetUrlId;
+            
+            psswdReset.setUrl(resetUrl);
+            
+            Mail.sendMail(user.getEmail(), resetUrl, user.getNickname());
+            request.setAttribute("error", false);
+        }
+        
+        RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/forgetPassword.jsp");
+        rd.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- Socia-Lite/Socia-Lite-war/web/forgetPassword.jsp
+++ Socia-Lite/Socia-Lite-war/web/forgetPassword.jsp
@@ -7,10 +7,10 @@
 </style>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<title> SociaLite - Login </title>
+<title> SociaLite - Forget Password </title>
 
 <%
-    Boolean errorLogin = (Boolean)request.getAttribute("errorLogin");
+    Boolean error = (Boolean)request.getAttribute("error");
 %>
     </head>
 <body>
@@ -24,28 +24,28 @@
     </div>
 
     <!-- Login Form -->
-    <form action="<%= request.getContextPath() %>/IndexServlet" method="POST">
+    <form action="<%= request.getContextPath() %>/ForgetPasswordServlet" method="POST">
       <input type="email" id="login" class="fadeIn second" name="email" placeholder="user@email.com">
-      <input type="password" id="password" class="fadeIn third" name="password" placeholder="password">
-      <input type="submit" class="fadeIn fourth" value="Log In">
+      <input type="submit" class="fadeIn fourth" value="Send email">
     </form>
 
-    <% if(errorLogin != null && errorLogin) { %>
+    <% if(error != null && error) { %>
     <div class="loginMessage alert alert-danger" role="alert">
         <a id="message">
-            Usuario o contrase&ntilde;a incorrecta
+            This mail is not associated with any account.
+        </a>
+    </div>
+    <% } else if(error != null && !error){ %>
+    <div class="loginMessage alert alert-success" role="alert">
+        <a id="message">
+            An email was sent. Check your inbox.
         </a>
     </div>
     <% } %>
 
     <!-- Remind Password -->
     <div id="formFooter">
-      <a class="underlineHover" href="<%= request.getContextPath() %>/forgetPassword.jsp">Forgot Password?</a>
-    </div>
-
-        <!-- Register -->
-    <div id="formFooter">
-      <a class="underlineHover" href="<%= request.getContextPath() %>/register.jsp">Register</a>
+      <a class="underlineHover" href="<%= request.getContextPath() %>/IndexServlet">Back to login</a>
     </div>
   </div>
 </div>
